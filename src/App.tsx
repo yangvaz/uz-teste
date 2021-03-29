46b5665d6c7d7b8283fbfbf8761b89849b3a1f94
Yan Garcia <yangarciavaz@gmail.com>
2021-03-28 23:19:13 -0300

    starting. base card created

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./styles.css";
+import bud from  "./images/budweiser.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+
+        <div className="card">
+           <img src={bud} />
+          <div className="description">
+            <h4> <strong> Budweiser </strong></h4>
+            <div className="infoProduct"> 16 Unit - 330ml Bottle icOn </div>
+            <div className="priceProduct"> R$ 43.00/Unit </div>
+            <div className="info">
+              For every 5 you buy, get up to 3 free Red Bull or 2 free Pepsi Black.
+              <div className="detailsLink"><a href=""> View details </a> </div>
+            </div>
+          </div>
+
+          <div className="footer">
+            <button className="btnRetirar"> - </button>
+            <label  />
+            <button className="btnAdicionar"> + </button>
+
+            <button className="btnUpdate"> UPDATE </button>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
